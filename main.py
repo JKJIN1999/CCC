f2aca0a5ae0f7e51abc26036b0b3ac57734c2007
JKJIN1999 <jugyeongkim911@gmail.com>
2024-03-27 18:47:32 +1100

    function works but need retouch of merge function

--- main.py
+++ main.py
@@ -2,14 +2,56 @@ from mpi4py import MPI
 import argparse
 import os
 import time
+import sys
 
+def maxFinder(dic):
+    if not dic:
+        return None, None
+    else:
+        max_key, max_value = max(dic.items(), key=lambda x: x[1])
+        return max_key, max_value
+
+def splitTweet(line):
+    date = None
+    hour = None
+    sentiment = None
+    if "created_at" in line:
+            date, hour = line.split('"created_at":"')[1].split('"',1)[0].split(":")[0].split("T")
+            hour = hour[:2]
+    if '"score"' not in line:
+        if '"sentiment"' in line:
+            sentiment = float(line.split('"sentiment":')[1].split('}')[0])
+    return date, hour, sentiment
+
+def mergeTweet(tweet_collected, key, sentiment):
+        if isinstance(sentiment, float):
+            if  bool(key) and bool(sentiment):
+                if key in tweet_collected:
+                    tweet_collected[key][0] += sentiment
+                    tweet_collected[key][1] += 1
+                else:
+                    tweet_collected[key] = [sentiment, 1]
+        elif isinstance(sentiment, list):
+            if  bool(key) and bool(sentiment):
+                if key in tweet_collected:
+                    tweet_collected[key][0] += sentiment[0]
+                    tweet_collected[key][1] += sentiment[1]
+                else:
+                    tweet_collected[key] = sentiment
+        return tweet_collected
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_path", type=str)
+    parser.add_argument("result_path", type=str)
+    return parser.parse_args()
 
 def main():
+    print("started")
     time_begin = time.time()
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
-    count = 0
     tweet_dict = {}
     
     args = getArgs()
@@ -19,128 +61,78 @@ def main():
     file_size = os.path.getsize(data_path)
     chunk_size = file_size / size
     chunk_start = int(chunk_size * rank)
-    generated_from_cut = 0
-    count_cut = 0
+
+    
 
     with open(data_path, encoding='utf-8') as file:
+
         file.seek(chunk_start)
         count_processed_chunk = 0
-        sentiment = None
-        created_at = None
-        tweet_id = None
-        line_cut = True
-        count = 0
+        if rank > 0:
+            line = file.readline()
+            count_processed_chunk += len(line.encode("utf-8"))
+
         while count_processed_chunk <= chunk_size:
-            count+= 1
+
+            #skip first line if rank is not 0 so we do not have any (the previous rank will read this line)
             line = file.readline()
             if not line:
                 break
-            ## Search if the current line is cut line 
-            if "}]}}," in line:
-                line_cut = False
-            else:
-                print(f"this is line for line with problem : {line}")
-                count_cut += 1
-                line_cut = True
-            if '"key":' in line and '"value"' in line:
-                tweet_id = line.split('"key":')[1].split('"value"')[0]
-            if "created_at" in line:
-                created_at = line.split('"created_at":"')[1].split('"',1)[0].split(":")[0]
-            if '"score"' not in line:
-                if '"sentiment"' in line:
-                    sentiment = line.split('"sentiment":')[1].split('}')[0]
-            
-            if bool(tweet_id) and bool(created_at) and bool(sentiment):
-                tweet_dict[tweet_id] = [created_at,sentiment]
-                if line_cut == True:
-                    print("all found but was cut so we move on")
-                line_cut = False
-                sentiment = None
-                created_at = None
-                tweet_id = None
-
+            date, hour, sentiment = splitTweet(line)
+            tweet_dict = mergeTweet(tweet_dict, date, sentiment)
+            tweet_dict = mergeTweet(tweet_dict, hour, sentiment)
             count_processed_chunk += len(line.encode("utf-8"))
              
-        if line_cut:
-            #extend line and find other  possible missing values
-            while True:
-                print(f"started to look for next line ")
-                line = file.readline()
-                if not line:
-                    break
-                print(f"this is line for extend line : {line}")
-                count += 1
-                if not bool(tweet_id) and '"key":' in line and '"value"' in line:
-                    tweet_id = line.split('"key":')[1].split('"value"')[0]
-                if not bool(created_at) and "created_at" in line:
-                    created_at = line.split('"created_at":"')[1].split('"',1)[0].split(":")[0]
-                if '"score"' not in line:
-                    if not bool(sentiment) and '"sentiment"' in line:
-                        sentiment = line.split('"sentiment":')[1].split('}')[0]
-                
-                if bool(tweet_id) and bool(created_at) and bool(sentiment):
-                    generated_from_cut += 1
-                    tweet_dict[tweet_id] = [created_at,sentiment]
-                    print(f"generated tweet dict : {created_at} and {sentiment}")
-                    break
-                else:
-                    count_cut+= 1
+        
+        #this line is the first line of the next rank
+
+        line = file.readline()
+        date, hour, sentiment = splitTweet(line)
+        tweet_dict = mergeTweet(tweet_dict, date, sentiment)
+        tweet_dict = mergeTweet(tweet_dict, hour, sentiment)
+    print("gathering")
 
     gathered_list = comm.gather(tweet_dict, root=0)
+    print("gathered")
+    gathered_tweet = {}
 
-    date_dict = {}
-    hour_dict = {}
-    c_date_dict = {}
-    c_hour_dict = {}
-
-    result = []
-    
-    if gathered_list != None:
-        print(len(gathered_list))
+    if gathered_list is not None:
+        
         for gathered_dict in gathered_list:
-            print(len(gathered_dict))
-            for collected_list in gathered_dict.values():
-                created_at, sentiment = collected_list
-                date, hour = created_at.split("T")
-                hour = hour[:2]
-                if date in date_dict:
-                    date_dict[date] += float(sentiment)
-                    c_date_dict[date] += 1
-                else:
-                    date_dict[date] = float(sentiment)
-                    c_date_dict[date] = 1
-                if hour in hour_dict:
-                    hour_dict[hour] += float(sentiment) 
-                    c_hour_dict[hour] += 1
-                else:
-                    hour_dict[hour] = float(sentiment)
-                    c_hour_dict[hour] = 1
+            for key, value_list in gathered_dict.items():
+                gathered_tweet = mergeTweet(gathered_tweet, key, value_list)
+    
+    # hour sentiment hour count date sentiment date count    
 
-    for dict in (date_dict,hour_dict,c_date_dict,c_hour_dict):
-        result.append(maxFinder(dict))
+    date_max_sentiment = {}
+    hour_max_sentiment = {}
+    date_max_count = {}
+    hour_max_count = {}
+
+    
+    for key, value_list in gathered_tweet.items():
+        if len(key) == 2:
+            hour_max_sentiment[key] = value_list[0]
+            hour_max_count[key] = value_list[1]
+        else:
+            date_max_sentiment[key] = value_list[0]
+            date_max_count[key] = value_list[1]
+            
+    key_date_sentiment, value_date_sentiment = maxFinder(date_max_sentiment)
+    key_date_count, value_date_count = maxFinder(date_max_count)
+    key_hour_sentiment, value_hour_sentiment = maxFinder(hour_max_sentiment)
+    key_hour_count, value_hour_count = maxFinder(hour_max_count)
     
     if rank == 0:
         with open(result_path, "w")  as f:
             time_end = time.time()
-            print(f"Date with the highest tweet count is {result[2][0]} with {result[2][1]} tweets\n", file = f)
-            print(f"Hour with the highest tweet count is {result[3][0]} with {result[3][1]} tweets\n", file = f)
-            print(f"The happiest date is {result[0][0]} with {result[0][1]} sentiment\n",file = f)
-            print(f"The happiest hour is {result[1][0]} with {result[1][1]} sentiment\n",file = f)
+            print(f"Date with the highest tweet count is {key_date_count} with {value_date_count} tweets\n", file = f)
+            print(f"Hour with the highest tweet count is {key_hour_count} with {value_hour_count} tweets\n", file = f)
+            print(f"The happiest date is {key_date_sentiment} with {value_date_sentiment} sentiment\n",file = f)
+            print(f"The happiest hour is {key_hour_sentiment} with {value_hour_sentiment} sentiment\n",file = f)
             print(f"Total run time = {time_end - time_begin}\n", file = f)
-
-def getArgs():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("data_path", type=str)
-    parser.add_argument("result_path", type=str)
-    return parser.parse_args()
-
-def maxFinder(dic):
-    if not dic:
-        return dic
-    max_key, max_value = max(dic.items(), key=lambda x: x[1])
-    return [max_key, max_value]
-    
- 
+    comm.barrier()
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
+    print("done")
     
--- test.py
+++ test.py
@@ -4,56 +4,129 @@ from collections import defaultdict
 import argparse
 import os
 import time
+def maxFinder(dic):
+    if not dic:
+        return None, None
+    else:
+        max_key, max_value = max(dic.items(), key=lambda x: x[1])
+        return max_key, max_value
+
+def splitTweet(line):
+    date = None
+    hour = None
+    sentiment = None
+    if "created_at" in line:
+            date, hour = line.split('"created_at":"')[1].split('"',1)[0].split(":")[0].split("T")
+            hour = hour[:2]
+    if '"score"' not in line:
+        if '"sentiment"' in line:
+            sentiment = float(line.split('"sentiment":')[1].split('}')[0])
+    return date, hour, sentiment
+
+def mergeTweet(tweet_collected, key, sentiment):
+        if isinstance(sentiment, float):
+            if  bool(key) and bool(sentiment):
+                if key in tweet_collected:
+                    tweet_collected[key][0] += sentiment
+                    tweet_collected[key][1] += 1
+                else:
+                    tweet_collected[key] = [sentiment, 1]
+        elif isinstance(sentiment, list):
+            if  bool(key) and bool(sentiment):
+                if key in tweet_collected:
+                    tweet_collected[key][0] += sentiment[0]
+                    tweet_collected[key][1] += sentiment[1]
+                else:
+                    tweet_collected[key] = sentiment
+        return tweet_collected
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_path", type=str)
+    parser.add_argument("result_path", type=str)
+    return parser.parse_args()
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-file_size = os.path.getsize("twitter-50mb.json")
-chunk_size = file_size / size
-
+file_size = os.path.getsize("twitter-1mb.json")
+chunk_size = file_size / 4
 chunk_start = int(chunk_size * rank)
 count_exist = 0
 count = 0
 count_not = 0
 tweet_dict = {}
-tweet_list = []
 missing_c = 0
 missing_s = 0
-start_time = time.time()
-with open("twitter-50mb.json", encoding='utf-8') as file:
+time_begin = time.time()
+
+
+
+with open("twitter-1mb.json", encoding='utf-8') as file:
+
     file.seek(chunk_start)
     count_processed_chunk = 0
+
     while count_processed_chunk <= chunk_size:
-        sentiment = None
-        created_at = None
+
+        #skip first line if rank is not 0 so we do not have any (the previous rank will read this line)
+        if rank != 0:
+            line = file.readline()
+            count_processed_chunk += len(line.encode("utf-8"))
+            continue
+
+        count+= 1
         line = file.readline()
         if not line:
             break
-        if "created_at" in line:
-            created_at = line.split('"created_at":"')[1].split('"',1)[0].split(":")[0]
-        else: missing_c +=1
-        if '"score"' not in line:
-            if '"sentiment"' in line:
-                sentiment = line.split('"sentiment":')[1].split('}')[0]
-            else: missing_s += 1                                         
-        else: missing_s += 1
-            
-        if bool(created_at) and bool(sentiment):
-            count += 1
-            if created_at in tweet_dict:
-                tweet_dict[created_at][0] += sentiment
-                tweet_dict[created_at][1] += 1
-            else:
-                tweet_dict[created_at] = [sentiment, 1]
-        
+
+        date, hour, sentiment = splitTweet(line)
+        tweet_dict = mergeTweet(tweet_dict, date, sentiment)
+        tweet_dict = mergeTweet(tweet_dict, hour, sentiment)
         count_processed_chunk += len(line.encode("utf-8"))
-tweet_list.append(tweet_dict)
-gathered_dict = comm.gather(tweet_list, root=0)
-print(gathered_dict)
-# print(count_exist)
-# print(gathered_dict)
-# print(len(gathered_dict))
-# print(count)
-# print(missing_c + missing_s)
-# print(time.time() - start_time)
+            
+    line = file.readline()
+    if line:
+        date, hour, sentiment = splitTweet(line)
+        tweet_dict = mergeTweet(tweet_dict, date, sentiment)
+        tweet_dict = mergeTweet(tweet_dict, hour, sentiment)
+
+gathered_list = comm.Gather(tweet_dict, root=0)
+gathered_tweet = {}
+
+if gathered_list is not None:
+    for gathered_dict in gathered_list:
+        for key, value_list in gathered_dict.items():
+            gathered_tweet = mergeTweet(gathered_tweet, key, value_list)
+
+# hour sentiment hour count date sentiment date count    
+
+date_max_sentiment = {}
+hour_max_sentiment = {}
+date_max_count = {}
+hour_max_count = {}
+
+
+for key, value_list in gathered_tweet.items():
+    if len(key) == 2:
+        hour_max_sentiment[key] = value_list[0]
+        hour_max_count[key] = value_list[1]
+    else:
+        date_max_sentiment[key] = value_list[0]
+        date_max_count[key] = value_list[1]
+
+
+key_date_sentiment, value_date_sentiment = maxFinder(date_max_sentiment)
+key_date_count, value_date_count = maxFinder(date_max_count)
+key_hour_sentiment, value_hour_sentiment = maxFinder(hour_max_sentiment)
+key_hour_count, value_hour_count = maxFinder(hour_max_count)
+
+if rank == 0:
+        with open("./results/test4.txt", "w")  as f:
+            time_end = time.time()
+            print(f"Date with the highest tweet count is {key_date_count} with {value_date_count} tweets\n", file = f)
+            print(f"Hour with the highest tweet count is {key_hour_count} with {value_hour_count} tweets\n", file = f)
+            print(f"The happiest date is {key_date_sentiment} with {value_date_sentiment} sentiment\n",file = f)
+            print(f"The happiest hour is {key_hour_sentiment} with {value_hour_sentiment} sentiment\n",file = f)
+            print(f"Total run time = {time_end - time_begin}\n", file = f)
